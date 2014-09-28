7142ecfcb4034ee1cf3554505ea99b157eeb0fc2
jirkaroz <jiri.roznovjak@gmail.com>
2014-09-28 03:01:43 -0400

    DONE

--- Refactored.py
+++ Refactored.py
@@ -42,7 +42,6 @@ class HangMan:
 		'''chooses a random word from the current topic'''
 		i = random.randint(0,len(self.wordList))
 		self.currentWord = (self.wordList[i-1])
-		print (self.currentWord)
 		
 	def newWord(self):
 		'''called after click on next word button. Deletes canvas, chooses new word'''
@@ -68,17 +67,14 @@ class HangMan:
 		'''initial window, selection of a topic at the start of the program'''
 		startWindow = Tk()
 		startWindow.title('Hang Man')
-		
 		frameOptions = LabelFrame(startWindow,text='Choose a Topic',labelanchor=N)
 		frameOptions.pack()
-		confirm = Button(frameOptions,text='OK',width=7,command=startWindow.destroy)
-		confirm.pack()
-		label = Label(startWindow)
-		label.pack()
 		self.topicVar = StringVar()
 		self.topicVar.set(None)
 		for word in self.options:
 			Radiobutton(frameOptions,text=word,variable=self.topicVar,value = word).pack()
+		confirm = Button(frameOptions,text='OK',width=7,command=startWindow.destroy)
+		confirm.pack()
 		geometry = '200x'+str(30+30*len(self.options))
 		startWindow.geometry(geometry)
 		startWindow.resizable(width=FALSE, height=FALSE)
@@ -86,13 +82,10 @@ class HangMan:
 	
 	def getTopic(self):
 		'''gets what topic I chose at the initial window.'''
-		self.topic = self.topicVar.get()
+		return self.topicVar.get()
 		
 	def runMainWindow(self):
 		'''runs up the main window. Need to call window mainloop separately after this'''
-		if self.topic == "None":
-			sys.exit(0) #if no topic chosen, closes the program
-		
 		self.mainWindow = Tk()
 		self.mainWindow.geometry('800x600')
 		self.mainWindow.resizable(width=FALSE, height=FALSE)
@@ -107,20 +100,20 @@ class HangMan:
 		self.wordField = Canvas(self.mainWindow, width = 590,height=120,bg = 'white',highlightthickness=0) #canvas with the unknown word
 		self.wordField.place(x=180,y=445)
 		
-		self.nextWord = Button(self.mainWindow,text = 'Next Word',command=self.newWord,highlightthickness=0) #next word button
+		self.nextWord = Button(self.mainWindow,text = 'Next Word',command=self.newWord,highlightthickness=0,state='disabled') #next word button
 		self.nextWord.place(x=20,y=15)
 		
 		entryFrame = LabelFrame(self.mainWindow,text = 'Enter a Letter',relief = FLAT,bg='#FFEBD6') #frame that contains field where the letters are entered
 		entryFrame.place(x = 45,y = 475)
 		
-		self.entryField = Entry(entryFrame,width=5,relief='sunken',font=('Times',14),state='normal')
+		self.entryField = Entry(entryFrame,width=5,relief='sunken',font=('Verdana',14),state='normal')
 		self.entryField.pack()
 		self.entryField.focus_set()
 		self.entryField.bind('<Return>',self.proceedLetterEvent) #need to implement the enter function first
 		
-		topicVar = StringVar(self.mainWindow)
-		topicVar.set(self.topic)
-		changeTopic = OptionMenu(self.mainWindow,topicVar,*self.options)
+		self.topicVar = StringVar(self.mainWindow)
+		self.topicVar.set(self.topic)
+		changeTopic = OptionMenu(self.mainWindow,self.topicVar,*self.options)
 		changeTopic.config(highlightthickness=0)
 		changeTopic.place(x=770,y=12,anchor=NE)
 		
@@ -183,8 +176,8 @@ class HangMan:
 					stateline = 'hidden'
 				else:
 					stateline = 'normal'
-				self.wordField.create_line(offset+n*(lineWidth+space),y,offset+n*(lineWidth+space)+lineWidth,y,width=1,state=stateline) #vytvoreni car pod pismeny
-				self.wordField.create_text(offset+n*(lineWidth+space)+lineWidth/2,y-l_h/2,text=word[n],font=('Times',height),state=state) #vytvoreni pismena    
+				self.wordField.create_line(offset+n*(lineWidth+space),y,offset+n*(lineWidth+space)+lineWidth,y,width=1,state=stateline) #lines under letters
+				self.wordField.create_text(offset+n*(lineWidth+space)+lineWidth/2,y-l_h/2,text=word[n],font=('Times',height),state=state) #letter 
 
 	def replaceCzech(self,word):
 		'''takes a word and replaces all czech letters with their english equivalent'''
@@ -198,7 +191,7 @@ class HangMan:
 		self.proceedLetter()
 
 	def proceedLetter(self):
-		'''proceeding the entered letter'''
+		'''proceeds the entered letter'''
 		word = self.currentWord
 		word_test = word.lower()
 		word_test = self.replaceCzech(word_test)
@@ -231,6 +224,7 @@ class HangMan:
 				self.endGame()
 				self.alreadyGuessed.extend(list(word))
 				self.drawLetters()
+				
 	
 	def drawHangman(self):
 		'''draws the picture of hangman'''
@@ -239,26 +233,31 @@ class HangMan:
 	def drawAlreadyGuessed(self):
 		'''draws already guessed letters'''
 		agprint = ', '.join(self.alreadyGuessedWrong)
-		self.hangmanCanvas.create_text(20,375,text=agprint,font = ('Times,14'),anchor = NW)
+		self.hangmanCanvas.create_text(20,375,text=agprint,font = ('Verdana,14'),anchor = NW)
 				
 	def youWin(self):
 		'''called after the whole word is guessed'''
-		self.hangmanCanvas.create_text(275,200,text='You win!',fill='red',font=('Times',28))
+		self.hangmanCanvas.create_text(275,200,text='You win!',fill='red',font=('Verdana',40))
 	
 	def youLose(self):
 		'''called after you run out of attempts'''
-		self.hangmanCanvas.create_text(275,200,text='Game Over',fill='red',font=('Times',28))
+		self.hangmanCanvas.create_text(275,200,text='Game Over',fill='red',font=('Verdana',40))
 
 	def endGame(self):
-		'''after the game ends, configures widgets'''
+		'''after the game ends, configures widgets and checks if the topic has been changed'''
 		self.nextWord.config(state='active')
 		self.entryField.config(state='disabled')
+		if self.topic != self.getTopic():
+			self.topic = self.getTopic()
+			self.loadWords()
 
 	def run(self):
 		'''method called at the end, wraps up the methods and runs the program'''
 		self.accessOptions()
 		self.runInitialWindow()
-		self.getTopic()
+		self.topic = self.getTopic()
+		if self.topic == "None":
+			sys.exit(0) #if no topic chosen, closes the program
 		self.loadWords()
 		self.randomWord()
 		self.runMainWindow()
@@ -266,11 +265,5 @@ class HangMan:
 		self.drawLetters()
 		self.mainWindow.mainloop()
 
-try:
-	game = HangMan()
-	game.run()
-except Exception as e:
-	print (e)
-
-
-input()
+game = HangMan()
+game.run()
